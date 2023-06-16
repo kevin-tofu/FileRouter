91ade4cc4fd41888ab93691271259d70209d36c1
Kevin-Tofu <85769623+kevin-tofu@users.noreply.github.com>
2023-06-17 08:37:10 +0900

    Update router.py

--- test/router.py
+++ test/router.py
@@ -14,73 +14,47 @@ class myProcessor(filerouter.processor):
     def __init__(self):
         super().__init__()
 
-    async def post_files_process(
-        self,
-        process_name: str,
-        files_org_info: list[dict],
-        file_dst_path: Optional[str] = None,
-        bgtask: BackgroundTasks=BackgroundTasks(),
-        **kwargs
-    ):
-        return dict(status = "OK")
-
-
     async def post_file_process(
         self,
         process_name: str,
-        file_org_info: list[dict],
+        files_org_info: dict,
         file_dst_path: Optional[str] = None,
         bgtask: BackgroundTasks=BackgroundTasks(),
         **kwargs
     ):
-        # print(fpath_org)
-        zipped_file_path_extact = os.path.splitext(file_org_info['path'])[0]
-        print('zipped_file_path_extact:', zipped_file_path_extact)
-        zippedFile_list = list()
-        for file_path in glob.glob(f"{zipped_file_path_extact}/*"):
-            print(file_path)
-            zippedFile_list.append(file_path)
+        if process_name == '':
+            return dict(status = "OK")
+        
+        elif pro == 'm':
+            zipped_file_path_extact = os.path.splitext(file_org_info['path'])[0]
+            print('zipped_file_path_extact:', zipped_file_path_extact)
+            zippedFile_list = list()
+            for file_path in glob.glob(f"{zipped_file_path_extact}/*"):
+                print(file_path)
+                zippedFile_list.append(file_path)
 
-        return dict(status = "ok", zippedFiles=zippedFile_list)
-
-
-    async def post_BytesIO_process(
-        self,
-        process_name: str,
-        file_org_info: dict,
-        file_dst_path: Optional[str] = None,
-        bgtask: BackgroundTasks=BackgroundTasks(),
-        **kwargs
-    ):
-
-        # do stuff
-        return dict(
-            filename=file_org_info['name'],
-            sentence=file_org_info["bytesio"].getvalue().decode('utf-8')
-        )
-
-    async def post_ListBytesIO_process(
-        self,
-        process_name: str,
-        files_org_info: list[dict],
-        file_dst_path: Optional[str] = None,
-        bgtask: BackgroundTasks=BackgroundTasks(),
-        **kwargs
-    ):
-        ret = list()
-        for data in files_org_info:
-            # data['bytesio'] # 
-            ret.append(
-                dict(
-                    filename=data['name'],
-                    sentence=data["bytesio"].getvalue().decode('utf-8')
-                )
+            return dict(status = "ok", zippedFiles=zippedFile_list)
+        elif aa == 'm':
+            return dict(
+                filename=file_org_info['name'],
+                sentence=file_org_info["bytesio"].getvalue().decode('utf-8')
             )
+        elif aa == '':
+             ret = list()
+            for data in files_org_info:
+                # data['bytesio'] # 
+                ret.append(
+                    dict(
+                        filename=data['name'],
+                        sentence=data["bytesio"].getvalue().decode('utf-8')
+                    )
+                )
 
-        return dict(info=ret)
+            return dict(info=ret)
 
+            
 
-
+    
 test_config = dict(
     PATH_DATA = "./temp"
 )
